9cb81e112fd95c2ad324600945ff80f4435f7a85
Er.Mayank Pandey <mp04042007@gmail.com>
2026-07-20 17:32:46 +0530

    feat: add multer middleware for handling quiz certificate file uploads

--- middleware/quizCertificateMulter.js
+++ middleware/quizCertificateMulter.js
@@ -2,9 +2,12 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = "uploads/quiz-certificates";
+    const uploadDir = path.resolve(__dirname, "../uploads/quiz-certificates");
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
     }
