c1f618385d9f60e620422c05eb04518f51598195
Er.Mayank Pandey <mp04042007@gmail.com>
2026-07-21 11:23:01 +0530

    feat: add express routes for support ticket management and admin operations

--- routes/supportTicket.routes.js
+++ routes/supportTicket.routes.js
@@ -6,7 +6,7 @@ import {
   deleteSupportTicket,
 } from '../controllers/supportTicket.controller.js';
 import optionalUserAuth from '../middleware/optionalUserAuth.js';
-import verifyAdmin from '../middleware/verifyAdmin.js';
+import verifyAdminToken from '../middleware/verifyAdminToken.js';
 
 const supportTicketRouter = express.Router();
 
@@ -14,8 +14,8 @@ const supportTicketRouter = express.Router();
 supportTicketRouter.post('/create', optionalUserAuth, createSupportTicket);
 
 // Admin Routes
-supportTicketRouter.get('/admin/all', verifyAdmin, getAllSupportTickets);
-supportTicketRouter.put('/admin/update/:id', verifyAdmin, updateSupportTicketStatus);
-supportTicketRouter.delete('/admin/delete/:id', verifyAdmin, deleteSupportTicket);
+supportTicketRouter.get('/admin/all', verifyAdminToken, getAllSupportTickets);
+supportTicketRouter.put('/admin/update/:id', verifyAdminToken, updateSupportTicketStatus);
+supportTicketRouter.delete('/admin/delete/:id', verifyAdminToken, deleteSupportTicket);
 
 export default supportTicketRouter;
