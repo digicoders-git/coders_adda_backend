4d8188814d7781a10682f3e734cf5cddd6c35a79
Er.Mayank Pandey <mp04042007@gmail.com>
2026-08-01 17:10:49 +0530

    Fix import path for userAuth in progress routes

--- routes/progress.routes.js
+++ routes/progress.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { updateProgressREST } from "../controllers/progress.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
+import userAuth from "../middleware/userAuth.js";
 
 const router = express.Router();
 
-router.post("/update", verifyToken, updateProgressREST);
+router.post("/update", userAuth, updateProgressREST);
 
 export default router;
