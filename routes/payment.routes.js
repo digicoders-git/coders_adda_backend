35910a005a39f5dc0e5efc0bc4f18b4153c5e0a3
Er.Mayank Pandey <mp04042007@gmail.com>
2026-08-11 11:20:07 +0530

    fix: resolve duplicate code in multiple files from bad merge

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -15,35 +15,3 @@ paymentRoute.post("/withdraw", userAuth, withdrawWallet);
 paymentRoute.get("/slip/:id", downloadTransactionSlip);
 
 export default paymentRoute;
-import express from "express";
-import userAuth from "../middleware/userAuth.js";
-import { createOrder, verifyPayment, recordPaymentFailure, payWithWallet, topupWallet, withdrawWallet } from "../controllers/payment.controller.js";
-import { freeEnroll } from "../controllers/enroll.controller.js";
-
-const paymentRoute = express.Router();
-
-paymentRoute.post("/create-order", userAuth, createOrder);
-paymentRoute.post("/verify", userAuth, verifyPayment);
-paymentRoute.post("/record-failure", userAuth, recordPaymentFailure);
-paymentRoute.post("/free", userAuth, freeEnroll);
-paymentRoute.post("/wallet-pay", userAuth, payWithWallet);
-paymentRoute.post("/topup", userAuth, topupWallet);
-paymentRoute.post("/withdraw", userAuth, withdrawWallet);
-
-export default paymentRoute;
-import express from "express";
-import userAuth from "../middleware/userAuth.js";
-import { createOrder, verifyPayment, recordPaymentFailure, payWithWallet, topupWallet, withdrawWallet } from "../controllers/payment.controller.js";
-import { freeEnroll } from "../controllers/enroll.controller.js";
-
-const paymentRoute = express.Router();
-
-paymentRoute.post("/create-order", userAuth, createOrder);
-paymentRoute.post("/verify", userAuth, verifyPayment);
-paymentRoute.post("/record-failure", userAuth, recordPaymentFailure);
-paymentRoute.post("/free", userAuth, freeEnroll);
-paymentRoute.post("/wallet-pay", userAuth, payWithWallet);
-paymentRoute.post("/topup", userAuth, topupWallet);
-paymentRoute.post("/withdraw", userAuth, withdrawWallet);
-
-export default paymentRoute;
